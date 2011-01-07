7476ebc96d312aba56c2571f9c46711cd15adc61
root <root@bruno.(none)>
2011-01-07 15:48:51 +0100

    mazani vice vyskytu en slova

--- en2cz.sh
+++ en2cz.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#set -x
 
 DICT_BIN=/usr/bin/dict
 
-CZ_WORD=$(${DICT_BIN} -deng-cze $1 | egrep -A 2 $1 | tr -d '\n')
+CZ_WORD="$1: $(${DICT_BIN} -deng-cze $1 | egrep -A 2 $1 | tr -d '\n' | sed "s/[^^]$1//g")"
 
 echo -e ${CZ_WORD}
