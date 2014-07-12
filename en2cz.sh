48e93ae7ef09bd14f6a8a250ee0ff6d9cd0ec8c0
Vaclav Strachon <wencaaa@gmail.com>
2014-07-12 19:50:06 +0200

    Update en2cz.sh

--- en2cz.sh
+++ en2cz.sh
@@ -3,6 +3,6 @@
 
 DICT_BIN=/usr/bin/dict
 
-CZ_WORD="$1: $(${DICT_BIN} -deng-cze $1 | egrep -A 2 $1 | tr -d '\n' | sed "s/[^^]$1//g")"
+CZ_WORD="$1: $(${DICT_BIN} -d fd-eng-cze $1 | egrep -i -A 2 $1 | tr -d '\n' | sed "s/[^^]$1//g")"
 
 echo -e ${CZ_WORD} | tee -a words.txt
